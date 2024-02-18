7098b1c5246ad6be502dcd0bf004fb6625bc7fb2
scottabrinton49 <scott@aluvii.com>
2024-02-17 22:04:29 -0700

    javascript testing changes

---
+++ myscript.js
@@ -0,0 +1,53 @@
+// #1
+
+// function calculateFahrenheit (celsius) {
+// return (celsius * 9/5) +32
+// };
+
+// console.log(calculateFahrenheit(0));
+
+// #2
+
+// let calculateFahrenheit = function(celsius) {
+// return (celsius * 9/5) + 32
+// };
+
+// console.log(calculateFahrenheit(40));
+
+// #3
+
+// let calculateFahrenheit = (celsius) => (celsius * 9/5) + 32;
+
+// console.log(calculateFahrenheit(40));
+
+// #4
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// let numbersSquared = numbers.map(num => num * num);
+
+// console.log(numbersSquared);
+
+// #5  
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// let filteredNumbers = numbers.filter(num => num % 2 === 0);
+
+// console.log(filteredNumbers);
+
+// console.log(numbers);
+
+// #6
+
+// let person = {
+//     fName: 'Scott',
+//     age: 40,
+//     eyes: 'blue'
+// };
+
+// console.log(person);
+
+// console.log(typeof person);
+
+// console.log(person.length);
