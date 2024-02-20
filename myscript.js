dbefeffeef3454eeabf033f42e0f46f9c584e0d6
scottabrinton49 <scott@aluvii.com>
2024-02-19 23:00:16 -0700

    javascript testing

--- myscript.js
+++ myscript.js
@@ -1,4 +1,4 @@
-// #1
+// #1  Write a function  //Weekly
 
 // function calculateFahrenheit (celsius) {
 // return (celsius * 9/5) +32
@@ -6,7 +6,7 @@
 
 // console.log(calculateFahrenheit(0));
 
-// #2
+// #2  Write an anonymous function //Weekly
 
 // let calculateFahrenheit = function(celsius) {
 // return (celsius * 9/5) + 32
@@ -14,13 +14,13 @@
 
 // console.log(calculateFahrenheit(40));
 
-// #3
+// #3  Write an arrow function  
 
 // let calculateFahrenheit = (celsius) => (celsius * 9/5) + 32;
 
 // console.log(calculateFahrenheit(40));
 
-// #4
+// #4  Use Map function  //Weekly
 
 // let numbers = [1, 2, 3, 4, 5];
 
@@ -28,7 +28,7 @@
 
 // console.log(numbersSquared);
 
-// #5  
+// #5  Use Filter Function //Weekly
 
 // let numbers = [1, 2, 3, 4, 5];
 
@@ -38,7 +38,7 @@
 
 // console.log(numbers);
 
-// #6
+// #6  Create an object //Weekly
 
 // let person = {
 //     fName: 'Scott',
@@ -51,3 +51,24 @@
 // console.log(typeof person);
 
 // console.log(person.length);
+
+//#7 Destructure an array;  //Daily
+
+// let names = ['adam', 'scott', 'eric', 'regan'];
+
+// const [sipherd, brinton,...others] = names;
+
+// console.log(others);
+
+//#8 useState //Daily 
+
+// const [hello, setHello] = useState('ola');
+
+// const handleClick = () => setHello('bonjour');
+
+// 2-18-2024  8/8 100% correct
+
+//2-19-2024
+
+//  1/2 50% correct because I put quotes on the variables brinton and sipherd when destructuring the array
+
